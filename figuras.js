74b27ffefc4d24663832b9b113bec5dbeaa20aef
Sandra <piko.soune10@gmail.com>
2021-09-19 01:12:26 -0500

    Estan integrados el círculo y los triángulos

--- figuras.js
+++ figuras.js
@@ -6,8 +6,9 @@ function perimetroCuadrado(lado) {
 
 function areaCuadrado(lado) {
   const area = lado * lado;
-  console.log("El area del cuadrado es: " + area);
+  return area;
 }
+
 
 // Triangulos
 function perimetroTriangulo(lado, lado2, base) {
@@ -32,9 +33,9 @@ function isosceles(lado1, lado2, base) {
     
   }
   if (lado1 === lado2 && base === lado2) {
-    alert("No es un triangulo isosceles, es equilatero");
+    alert("No es un triangulo isósceles, es equilátero");
   } else {
-    alert("No es un triangulo isosceles");
+    alert("No es un triangulo isósceles, es escaleno");
   }
 }
 //circulo
@@ -49,4 +50,71 @@ function areaCirculo(radio) {
   const PI = Math.PI;
   const area = radio ** 2 * PI;
   return area;
+}
+
+
+//Interacción con html
+
+//Cuadrado
+function calculaCuadradoPer() {
+  const input = document.getElementById("inputCua");
+  const valor = input.value;
+  const calculo = perimetroCuadrado(valor);
+  const resultadoPerCua = document.getElementById("resultadoPerCua");
+  resultadoPerCua.innerText = "El perímetro de tu cuadrado es " + calculo + "cm";
+}
+
+function calculaCuadradoArea() {
+  const input = document.getElementById("inputCua");
+  const valor = input.value;
+  const calculo = areaCuadrado(valor);
+  
+  const resultadoAreCua = document.getElementById("resultadoAreCua");
+  resultadoAreCua.innerText = "El área de tu cuadrado es " + calculo + "cm^2" ;
+ 
+}
+
+//Triangulos
+function calculaPerimetroTriangulo(){
+  const inputL1 = document.getElementById("inputLado1");
+  const inputL2 = document.getElementById("inputLado2");
+  const inputB = document.getElementById("inputBase");
+  const valorL1 = Number(inputL1.value);
+  const valorL2 = Number(inputL2.value);
+  const valorB = Number(inputB.value);
+  const calculo = perimetroTriangulo(valorL1, valorL2, valorB);
+  const resultadoPerTrian = document.getElementById("resultadoPerTrian");
+  resultadoPerTrian.innerText = "El perímetro de tu triángulo es " + calculo + "cm";
+}
+
+function calculaAreaTriangulo(){
+  const inputAltura = document.getElementById("inputAltura");
+  const inputB = document.getElementById("inputBase");
+  const valorAltura = Number(inputAltura.value);
+  const valorB = Number(inputB.value);
+  const calculo = areaTriangulo(valorB, valorAltura);
+  const resultadoAreTrian = document.getElementById("resultadoAreTrian");
+  resultadoAreTrian.innerText = "El área de tu triángulo es " + calculo + "cm^2";
+}
+
+function calculaAlturaIsosceles(){
+  const inputL1 = document.getElementById("inputLado1");
+  const inputL2 = document.getElementById("inputLado2");
+  const inputB = document.getElementById("inputBase");
+  const valorL1 = Number(inputL1.value);
+  const valorL2 = Number(inputL2.value);
+  const valorB = Number(inputB.value);
+  const calculo = isosceles(valorL1, valorL2, valorB);
+  const resultadoAltura = document.getElementById("resultadoAltura");
+  resultadoAltura.innerText = "La altura de tu triángulo isósceles es " + calculo + "cm";
+}
+
+//Círculo
+
+function calculaCirculoPer() {
+  const input = document.getElementById("inputCir");
+  const valor = Number(input.value);
+  const calculo = circunferencia(valor);
+  const resultadoPerCir = document.getElementById("resultadoPerCir");
+  resultadoPerCir.innerText = "La circunferencia de tu círculo es " + calculo + "cm";
 }
